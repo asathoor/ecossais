0f9795d6589e5d3e4ab61b4dfaa5c65f304cc5c3
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2019-05-07 11:00:35 +0200

    jquery ,v

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/css.css">
+  <title>Forskningsseminar Odense 18. - 19. maj 2019</title>
+</head>
+
+<body>
+
+  <header>
+    <h1>Forskningsseminar Maj 2019</h1>
+    <h2>Stammer S&therefore; L&therefore; fra Skotland?</h2>
+    <h3>Per Thykjær Jensen, X DM FRMÜ</h3>
+  </header>
+
+<nav>
+  <ul>
+    <li><a href="ecossais.html">St. Andrew Lodges</a></li>
+    <li><a href="so.html">Rite of the Strict Observance</a></li>
+    <li><a href="ra.html">Royal Arch</a></li>
+  </ul>
+</nav>
+
+  <footer>
+    &copy; Per Thykjær Jensen, <?php echo date("Y"); ?>
+  </footer>
+
+</body>
+
+</html>
